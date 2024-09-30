37148d6d56eb87d3ae185e92e473e7e9b69b5bda
yamesant <165981998+yamesant@users.noreply.github.com>
2024-09-30 11:11:38 +1000

    Customise tasks view

--- RTM.UI/ViewModels/TaskViewModel.cs
+++ RTM.UI/ViewModels/TaskViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using RTM.Core;
 
 namespace RTM.UI.ViewModels;
@@ -5,4 +6,12 @@ namespace RTM.UI.ViewModels;
 public class TaskViewModel(Task task) : ViewModelBase
 {
     public string Name => task.Name;
+    public TaskSchedulingStatus SchedulingStatus => task.SchedulingStatus;
+    public bool IsOverdue => SchedulingStatus == TaskSchedulingStatus.Overdue;
+    public bool IsReady => SchedulingStatus == TaskSchedulingStatus.Ready;
+    public bool IsScheduled => SchedulingStatus == TaskSchedulingStatus.Scheduled;
+    public string CompletionInfo => $"Completion count: {task.CompletionCount}";
+    public string IntervalInfo => $"Interval target: {task.IntervalTarget}";
+    public string ReadyInfo => $"Days until ready: {Math.Max(0, task.LowerTarget - task.DaysSinceLastCompletion)}";
+    public string OverdueInfo => $"Days until overdue: {Math.Max(0, task.UpperTarget - task.DaysSinceLastCompletion)}";
 }
