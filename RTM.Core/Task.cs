dab9a0b76029be54210c1e2f92319c52dc232f2a
yamesant <165981998+yamesant@users.noreply.github.com>
2024-09-30 10:32:26 +1000

    Add task class

---
+++ RTM.Core/Task.cs
@@ -0,0 +1,25 @@
+﻿namespace RTM.Core;
+
+public class Task
+{
+    public Guid Id { get; protected set; }
+    public string Name { get; protected set; }
+    public int CompletionCount { get; protected set; }
+    public DateTime TaskCreationDate { get; protected set; }
+    public DateTime? LastCompletionDate { get; protected set; }
+    public int IntervalTarget { get; protected set; }
+
+    protected Task()
+    {
+        
+    }
+    public Task(string name, int intervalTarget)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        CompletionCount = 0;
+        TaskCreationDate = DateTime.UtcNow;
+        LastCompletionDate = null;
+        IntervalTarget = intervalTarget;
+    }
+}
